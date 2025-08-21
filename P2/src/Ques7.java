92d30659222e2baa3a70721c8759f667d6135cd9
Damodar Tirle <damodar.tirole04@gmail.com>
2025-08-21 11:27:29 +0530

    Some question solved p2

--- P2/src/Ques7.java
+++ P2/src/Ques7.java
@@ -1,2 +1,12 @@
+import java.util.stream.IntStream;
+
 public class Ques7 {
+    //Multiply 1st and last and so on in Array
+    public static void main(String[] args) {
+        int arr[] = {2,4,5,4,2,6};
+
+        IntStream.range(0, arr.length/2)
+                .map(x-> arr[x] * arr[arr.length-x-1])
+                .forEach(System.out :: println);
+    }
 }
