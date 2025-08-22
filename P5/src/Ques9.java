dec3e35e5a1355efd004a768f9f04ca14a23fb1f
Damodar Tirle <damodar.tirole04@gmail.com>
2025-08-22 14:00:56 +0530

    Some question solved p5

--- P5/src/Ques9.java
+++ P5/src/Ques9.java
@@ -1,2 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Ques9 {
+    //List to Array
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Abc","Xyz","Qwe","Ert");
+
+        String[] str = list.stream()
+                .toArray(String[]::new);
+
+        for (String s:str){
+            System.out.println(s);
+        }
+    }
 }
