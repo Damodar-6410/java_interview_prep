71ad5cb9d37febc172dd358ae0c384e484a753d7
Damodar Tirle <damodar.tirole04@gmail.com>
2025-08-22 19:02:21 +0530

    Some question solved p5

---
+++ P6/src/Ques10.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Ques10 {
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<>();
+        list.add(new Employee("Abc", "IT",12000));
+        list.add(new Employee("Pqe", "RTO",23000));
+        list.add(new Employee("Sqw", "IT",25000));
+        list.add(new Employee("Qwe", "IT",40000));
+        list.add(new Employee("Uyt", "RTO",32000));
+        list.add(new Employee("Pou", "EY",50000));
+        list.add(new Employee("Pou", "EY",40000));
+
+        Map<String, Double> collect = list.stream()
+                .collect(Collectors.toMap(
+                        Employee::getName,
+                        Employee::getSalary,
+                        (a, b) -> b
+                ));
+        System.out.println(collect);
+
+    }
+}
