158d0e6ff0a8cbaf3a3f3fed6e7825f06f102803
Damodar Tirle <damodar.tirole04@gmail.com>
2025-08-22 14:59:52 +0530

    Some question solved p5

--- P6/src/Ques1.java
+++ P6/src/Ques1.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 public class Ques1 {
+    //Mid-character of String
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String s = sc.next();
+        int length = s.length();
+        int mid = length/2;
+
+        String collect = IntStream.range(0, length)
+                .filter(x -> length % 2 == 0 ? x == mid || x == mid - 1 : x == mid)
+                .mapToObj(s::charAt)
+                .collect(StringBuilder::new,
+                        StringBuilder::append,
+                        StringBuilder::append)
+                .toString();
+
+        System.out.println(collect);
     }
 }
