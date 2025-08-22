a6a45f43538832c5df257bbc98e337ada210806c
Damodar Tirle <damodar.tirole04@gmail.com>
2025-08-22 18:19:53 +0530

    Some question solved p5

--- P6/src/Ques3.java
+++ P6/src/Ques3.java
@@ -3,48 +3,15 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 //Print the Employees whose salary filtered and modified
-class Employee{
-    private String name;
-    private double salary;
-
-    public Employee(String name, double salary) {
-        this.name = name;
-        this.salary = salary;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "name='" + name + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
-}
 public class Ques3 {
     public static void main(String[] args) {
 
         List<Employee> list = new ArrayList<>();
-        list.add(new Employee("Abc",12000));
-        list.add(new Employee("Xyz",23000));
-        list.add(new Employee("Pqr",35000));
-        list.add(new Employee("Wte",15000));
-        list.add(new Employee("Dre",40000));
+        list.add(new Employee("Abc","IT",12000));
+        list.add(new Employee("Xyz","IT",23000));
+        list.add(new Employee("Pqr","RTO",35000));
+        list.add(new Employee("Wte","IT",15000));
+        list.add(new Employee("Dre","EY",40000));
 
         list.stream()
                 .filter(x -> x.getSalary() > 20000)
--- P6/src/Ques6.java
+++ P6/src/Ques6.java
@@ -3,46 +3,20 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-class Emp {
-    private String name;
-    private String department;
-
-    public Emp(String name, String department) {
-        this.name = name;
-        this.department = department;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-}
-
 public class Ques6 {
     //Find the department with maximum people
     public static void main(String[] args) {
-        List<Emp> list = new ArrayList<>();
-        list.add(new Emp("Abc", "IT"));
-        list.add(new Emp("Pqe", "RTO"));
-        list.add(new Emp("Sqw", "IT"));
-        list.add(new Emp("Qwe", "IT"));
-        list.add(new Emp("Uyt", "RTO"));
-        list.add(new Emp("Pou", "EY"));
+        List<Employee> list = new ArrayList<>();
+        list.add(new Employee("Abc", "IT",12000));
+        list.add(new Employee("Pqe", "RTO",23000));
+        list.add(new Employee("Sqw", "IT",25000));
+        list.add(new Employee("Qwe", "IT",40000));
+        list.add(new Employee("Uyt", "RTO",32000));
+        list.add(new Employee("Pou", "EY",50000));
 
         Map<String, Long> map = list.stream()
                 .collect(Collectors
-                        .groupingBy(Emp::getDepartment,
+                        .groupingBy(Employee::getDepartment,
                                 Collectors.counting()));
 
         Map.Entry<String, Long> stringLongEntry = map.entrySet().stream()
