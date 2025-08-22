6bca4c7c6f0780e8d26f1c4b87172dde8b9f2ead
Damodar Tirle <damodar.tirole04@gmail.com>
2025-08-22 18:43:10 +0530

    Some question solved p5

---
+++ P6/src/Ques9.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Ques9 {
+    //Convert list of string into map of String and its equivalent length
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Banana","Mango","Apple","Orange");
+
+        Map<String, Integer> collect = list.stream()
+                .collect(Collectors.toMap(
+                        x -> x,
+                        x -> x.length()
+                ));
+        System.out.println(collect);
+    }
+}
